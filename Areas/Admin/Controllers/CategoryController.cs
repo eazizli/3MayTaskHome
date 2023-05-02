240c090911968a73dcd111128a8a53dc08de9d9c
Elvin Ezizli <elvin@example.com>
2023-05-03 03:21:52 +0400

    .

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using EvaraMVC.DataContext;
+using EvaraMVC.Modals;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EvaraMVC.Areas.Admin.Controllers
+{
+        [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly EvaraDbContext _evardbContext;
+        private EvaraDbContext _evarDbContext;
+
+        public CategoryController(EvaraDbContext dbContext)
+        {
+            _evarDbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Category> categories=await _evardbContext.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
